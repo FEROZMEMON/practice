e5a3ecd819aeac60f4a0b79ceb8bbb7f5b90fdff
ferozmemon <ferozmemon726@gmail.com>
2024-02-04 17:32:49 +0500

    practice js

---
+++ app.js
@@ -0,0 +1,25 @@
+let feroz = "developer"
+const hussain = "not a developer"
+var karachi = "city"
+var islamabad = "light"
+var namaz = "muslim"
+
+
+console.table([feroz, karachi, islamabad , namaz
+
+])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
