4163f117130dc42f8b87352e175b75d844e968b3
ferozmemon <ferozmemon726@gmail.com>
2024-09-13 19:55:24 +0500

    changes

--- app.js
+++ app.js
@@ -1,25 +1,118 @@
-let feroz = "developer"
-const hussain = "not a developer"
-var karachi = "city"
-var islamabad = "light"
-var namaz = "muslim"
+// const input = document.querySelector("#input")
 
+let a = "feroz"
 
-console.table([feroz, karachi, islamabad , namaz
+let b = "feroz2"
 
-])
+let c = "feroz3"
 
-
+let d = "feroz4"
 
 
 
 
-
+console.log(a,b,c,d);
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let input = document.querySelector(".todo")
+// let ol = document.querySelector("ol")
+
+
+
+// let todo = []
+
+// function addTodo(e) {
+//     e.preventDefault();
+
+//     if (input.value == "") {
+//         alert("enter the task")
+
+//     }
+//     else {
+
+
+//         ol.innerHTML = ""
+
+//         todo.push(input.value)
+
+//         console.log(todo);
+
+//         for (let i = 0; i < todo.length; i++) {
+
+//             ol.innerHTML += `<li> ${todo[i]} </li> <button onclick="deleteTodo(${i})" >Delete</button> 
+//         <button onclick="editTodo(${i})">Edit</button> `
+
+//         }
+
+//         input.value = ""
+//     }
+// }
+// function deleteTodo(i) {
+//     ol.innerHTML = ""
+//     todo.splice(i, 1)
+
+//     for (let i = 0; i < todo.length; i++) {
+
+//         ol.innerHTML += `<li> ${todo[i]} </li> 
+//     <button onclick="deleteTodo(${i})" >Delete</button> 
+//     <button onclick="editTodo(${i})">Edit</button> `
+//     }
+
+// }
+
+
+// function editTodo(i) {
+//     ol.innerHTML = ""
+
+//     let user = prompt("Edit the list")
+
+//     todo.splice(i, 1, user)
+//     for (let i = 0; i < todo.length; i++) {
+
+//         ol.innerHTML += `<li> ${todo[i]} </li> 
+//      <button onclick="deleteTodo(${i})" >Delete</button> 
+//      <button onclick="editTodo(${i})">Edit</button> `
+
+//     }
+
+// }
+
+// console.log(todo);
 
 
